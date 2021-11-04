ff253349b8fa0b53b5676f0db04e7f459fbfa04b
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 11:28:28 +0300

    modify login route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitFieldBooleanField,ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
