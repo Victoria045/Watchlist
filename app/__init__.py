b86a6b4d8abffd9353535f611cc58ba99f417b29
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 12:06:53 +0300

    import and initialize the Mail class from flask_mail extension

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 # from .config import DevConfig
 
 # Initializing application
@@ -22,14 +23,16 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 # from app import views
 # from app import error
 
-
-bootstrap = Bootstrap()
-db = SQLAlchemy()
-
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
 
 def create_app(config_name):
 
@@ -44,6 +47,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app) 
     login_manager.init_app(app)
+    mail.init_app(app)
     # db = SQLAlchemy(app)  
 
     # Registering the blueprint
