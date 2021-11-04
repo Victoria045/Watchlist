795f45f1b65b3e792605f972a83ebd30f7284d15
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 11:27:50 +0300

    upload the uploaded photos file path

--- config.py
+++ config.py
@@ -15,6 +15,8 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://victoria:pass@localhost/watchlist'
 
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
