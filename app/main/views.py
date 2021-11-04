38a22b8532e86d3e224a1966c2ffc1c2bcb00da4
Victoria045 <victoriaberyl45@gmail.com>
2021-11-04 08:06:29 +0300

    crate update_profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,8 @@ from flask_login import login_required
 # from app import app
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from .forms import ReviewForm
+from .forms import ReviewForm, UpdateProfile
+from .. import db
 from ..models import Review, user
 
 
@@ -91,3 +92,23 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
